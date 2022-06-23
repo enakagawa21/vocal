49e1c6444e0b9b358be6a4bc2b6de37620b7d291
Eric Nakagawa <ericnakagawa@EricsMacStudio.hsd1.ca.comcast.net>
2022-06-23 00:27:05 -0700

    added image slider

--- src/components/doors/Data.js
+++ src/components/doors/Data.js
@@ -1,50 +1,74 @@
 export const cardData = [
 {
     id:1,
-    title: 'Roll-Up Doors',
-    linkImg: 'This will be image',
+    title: 'Automatic',
+    linkImg: 'images/automaticDoors/automaticSlidingDoor.jpeg',
     description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
 },
 {
     id:2,
-    title: 'Overhead Doors',
-    linkImg: 'test',
+    title: 'Commercial Roll-Up',
+    linkImg: 'images/commercialRollUpDoors/rollingSteelServiceDoor.jpeg',
     description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
 },
-{
+{        
     id:3,
-    title: 'Roll-Up Doors',
-    linkImg: 'This will be image',
+    title: 'Garage',
+    linkImg: 'images/GarageDoors/garageGlassDoors2.jpeg',
     description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
 },
 {
     id:4,
     title: 'Roll-Up Doors',
-    linkImg: 'This will be image',
+    linkImg: 'images/AutomaticDoors/AutoSwingDoors.jpeg',
     description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
 },
 {
     id:5,
     title: 'Roll-Up Doors',
-    linkImg: 'This will be image',
+    linkImg: 'images/AutomaticDoors/AutoSwingDoors.jpeg',
     description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
 },
 {
     id:6,
     title: 'Roll-Up Doors',
-    linkImg: 'This will be image',
+    linkImg: 'images/AutomaticDoors/AutoSwingDoors.jpeg',
     description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
 },
 {
     id:7,
     title: 'Roll-Up Doors',
-    linkImg: 'This will be image',
+    linkImg: 'images/AutomaticDoors/AutoSwingDoors.jpeg',
     description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
 },
 {
     id:8,
     title: 'Roll-Up Doors',
-    linkImg: 'This will be image',
+    linkImg: 'images/AutomaticDoors/AutoSwingDoors.jpeg',
+    description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
+},
+{
+    id:9,
+    title: 'Roll-Up Doors',
+    linkImg: 'images/AutomaticDoors/AutoSwingDoors.jpeg',
+    description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
+},
+{
+    id:10,
+    title: 'Roll-Up Doors',
+    linkImg: 'images/AutomaticDoors/AutoSwingDoors.jpeg',
+    description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
+},
+{
+    id:11,
+    title: 'Roll-Up Doors',
+    linkImg: 'images/AutomaticDoors/AutoSwingDoors.jpeg',
+    description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
+},
+{
+    id:12,
+    title: 'Roll-Up Doors',
+    linkImg: 'images/AutomaticDoors/AutoSwingDoors.jpeg',
     description: 'This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions'
 },
 
--- src/components/doors/Doors.js
+++ src/components/doors/Doors.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import './Doors.css'
+import React from "react";
+import "./Doors.css";
 
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { cardData } from './Data';
-import Slider from 'react-slick'
-import { FaAngleRight, FaAngleLeft } from 'react-icons/fa';
-
+import { cardData } from "./Data";
+import Slider from "react-slick";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
+import { useRef } from "react";
 
 function Doors() {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: false,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     initialSlide: 0,
     responsive: [
       {
@@ -23,62 +23,59 @@ function Doors() {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 960,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
+  const sliderRef = useRef(null);
+  console.log(sliderRef.current);
+
   return (
- 
-<div className='doors' id='services'>
+    <div className="doors" id="services">
+      <div className="header">
+        <h1>SERVICES</h1>
+      </div>
+      <div className="buttons">
+        <div className="prev-btn" onClick={() => sliderRef.current.slickPrev()}>
+          Prev
+        </div>
+        <div className="next-btn" onClick={() => sliderRef.current.slickNext()}>
+          Next
+        </div>
+      </div>
 
-<div className='header'>
-  <h1>SERVICES</h1>
-</div>
-<div className='buttons'>
-
-<div className='prev-btn'>
-<FaAngleLeft />
-</div>
-<div className='next-btn'>
-  <FaAngleRight />
-  </div>
-
-</div>
-
-<Slider{...settings}>
-
-    {cardData.map(item=>(
-<div className='card'>
-  <div className='card-top'>
-    <img src = {item.linkImg}/>
-    <h3>{item.title}</h3>
-  </div>
-  <div className='card-bottom'>
-    <p className='description'>{item.description}</p>
-  </div>
-</div>
-
-  ))}
-</Slider>
-
-  </div>
-  )
+      <Slider ref={sliderRef} {...settings}>
+        {cardData.map((item) => (
+          <div className="card">
+            <div className="card-top">
+              <img src={item.linkImg} />
+  
+            </div>
+            <div className="card-bottom">
+            <h3>{item.title}</h3>
+              <p className="description">{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
-export default Doors
+export default Doors;
