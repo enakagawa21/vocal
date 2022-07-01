d8d140efdea5e46258ca8273ffa55275b1faf53d
enakagawa21 <eric_nakagawa@yahoo.com>
2022-07-01 01:25:35 -0700

    hero section slide show content updated with alternate backgrounds

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,13 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Hero.css";
 import { heroData } from "./HeroData";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 const Hero = () => {
   const [currentSlide, setcurrentSlide] = useState(0);
+  const slideLength = heroData.length;
+
+  const autoPlay = true;
+  let slideInterval;
+  let IntervalTime = 9999;
+
+  const nextSlide = () => {
+    setcurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+  };
+  const prevSlide = () => {
+    setcurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+  };
+
+  function auto() {
+    slideInterval = setInterval(nextSlide, IntervalTime);
+  }
+
+  useEffect(() => {
+    setcurrentSlide(0);
+  }, []);
+
+  useEffect(() => {
+    if (autoPlay) {
+      auto();
+    }
+    return () => clearInterval(slideInterval);
+  }, [currentSlide]);
 
   return (
     <div className='hero-slider'>
+      <div>
+        <AiOutlineArrowLeft className='arrow prev' onClick={prevSlide} />
+      </div>
+      <div>
+        <AiOutlineArrowRight className='arrow next' onClick={nextSlide} />
+      </div>
+
       {heroData.map((slide, index) => {
         return (
           <div
@@ -16,7 +50,11 @@ const Hero = () => {
             {index === currentSlide && (
               <div>
                 <img src={slide.linkImg} alt={slide.alt} />
-                <div className='content'>
+                <div
+                  className={
+                    slide.lightTxt === true ? "white-content" : "content"
+                  }
+                  key={slide.id}>
                   <h1>{slide.title}</h1>
                   <p>{slide.description}</p>
                   <hr />
--- src/components/hero/HeroData.js
+++ src/components/hero/HeroData.js
@@ -1,18 +1,21 @@
 export const heroData = [
   {
     id: 1,
+    lightTxt: false,
     title: "Proudly Serving All of California",
     description: "This should change as well...",
-    linkImg: "images/blue-door.jpg",
+    linkImg: "images/blue-door4.jpg",
   },
   {
     id: 2,
+    lightTxt: true,
     title: "I want this to change",
     description: "Second slide......",
     linkImg: "images/img-5.jpg",
   },
   {
     id: 3,
+    lightTxt: true,
     title: "this is now changing",
     description: "changing one slide at at time...",
     linkImg: "images/img-1.jpg",
