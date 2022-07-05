163d240261105df55bd4925e53a3561a4a48662d
enakagawa21 <eric_nakagawa@yahoo.com>
2022-07-05 15:40:16 -0700

    services card description updated

--- src/components/doors/Data.js
+++ src/components/doors/Data.js
@@ -4,41 +4,41 @@ export const cardData = [
         title: "Commercial Roll-Up",
         linkImg: "images/commercialRollUpDoors/rollingSteelServiceDoor.jpeg",
         description:
-            "Rolling Steel Doors, Sectional Doors, Sheet and storage Unit Doors, Rolling Aluminum Grilles, Sliding Aluminum Grilles",
+            "NOTE: DISPLAY IN LIST FORM ADD LINK, Rolling Steel Doors, Sectional Doors, Sheet and storage Unit Doors, Rolling Aluminum Grilles, Sliding Aluminum Grilles",
     },
     {
         id: 2,
         title: "Glass Aluminum Entries",
         linkImg: "images/storefrontAndGlassAluminumDoors/aluminumGlassDoors.webp",
         description:
-            "This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions",
+            "NOTE: DISPLAY IN LIST FORM ADD LINK, Panic Glass Aluminum doors, All Glass Herculite Doors, Glass Sliding Doors, Storefronts",
     },
     {
         id: 3,
         title: "Metal and Wood Doors",
         linkImg: "images/storefrontAndGlassAluminumDoors/aluminumGlassDoors.webp",
         description:
-            "This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions",
+            "NOTE: DISPLAY IN LIST FORM ADD LINK, Hollow Metal Doors, Solid Core Wood Doors, Laminated Wood Doors, Primed Coat Wood Doors",
     },
     {
         id: 4,
         title: "Specialty Doors",
         linkImg: "images/hollowMetalDoors/homepageNewDoorsSet.jpeg",
         description:
-            "This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions",
+            "NOTE: DISPLAY IN LIST FORM ADD LINK, Stainless Steel Doors, Fiber Glass Doors, Impact Doors, Fire Rated Metal and Wood Doors",
     },
     {
         id: 5,
         title: "Automatic Doors",
         linkImg: "images/storefrontAndGlassAluminumDoors/aluminumGlassDoors.webp",
         description:
-            "This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions",
+            "NOTE: DISPLAY IN LIST FORM ADD LINK, Automatic Sliding Doors, Automatic Swing Doors, Automatic Folding Doors, Automatic Telescopic Doors",
     },
     {
         id: 6,
         title: "Gates",
         linkImg: "images/storefrontAndGlassAluminumDoors/aluminumGlassDoors.webp",
         description:
-            "This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptions This will be descriptionsThis will be descriptionsThis will be descriptions",
+            "NOTE: DISPLAY IN LIST FORM ADD LINK, Wrought Iron and Chain Link Gates, Sliding and Swing gates, Pedestrian",
     },
 ];
